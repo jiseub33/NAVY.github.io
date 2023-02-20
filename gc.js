52cddcea29b144d6e2814114fb137ff77392c125
jiseub33 <jiseub33@gmail.com>
2023-02-20 13:31:21 +0900

    test-update

--- gc.js
+++ gc.js
@@ -7,3 +7,19 @@ moveScrollright = function() {
 	var _scrollX = $('.shortcuts').scrollLeft();
 	$('.shortcuts').scrollLeft(_scrollX - 200);
 };
+
+let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
