0d4ca7a7bd70e0a1b9294376941bd6ccbb2e4561
jiseub33 <jiseub33@gmail.com>
2023-05-21 01:59:47 +0900

    update

--- join/device.js
+++ join/device.js
@@ -1,15 +1,22 @@
 // Check if the device is a mobile device
 function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-  }
-  
-  // Apply styles based on device type
-  if (isMobileDevice()) {
-    // Mobile styles
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+// Apply styles based on device type
+var deviceButton = document.getElementById("deviceButton");
+if (isMobileDevice()) {
+  // Mobile styles
+  deviceButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default behavior of the link
     document.getElementById("mobile").style.display = "block";
     document.getElementById("pc").style.display = "none";
-  } else {
-    // PC styles
+  });
+} else {
+  // PC styles
+  deviceButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default behavior of the link
     document.getElementById("mobile").style.display = "none";
     document.getElementById("pc").style.display = "block";
-  }  
+  });
+}
