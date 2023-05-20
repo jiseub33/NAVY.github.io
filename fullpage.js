46ff5b1bd0d42ad8412b09797b7524f8a4e7f5e5
jiseub33 <jiseub33@gmail.com>
2023-05-21 02:06:57 +0900

    update

--- fullpage.js
+++ fullpage.js
@@ -1,11 +1,28 @@
 function toggleFullScreen() {
-    var button = document.getElementById("fullscreenButton");
-    if (document.fullscreenElement) {
+  var button = document.getElementById("fullscreenButton");
+  var element = document.documentElement; // Change the target element here (e.g., document.getElementById("targetElement"))
+
+  if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+    if (document.exitFullscreen) {
       document.exitFullscreen();
-      button.classList.remove("active");
-    } else {
-      document.documentElement.requestFullscreen();
-      button.classList.add("active");
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
     }
+    button.classList.remove("active");
+  } else {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+    button.classList.add("active");
   }
-  
+}
--- join/device.js
+++ join/device.js
@@ -1,22 +1,15 @@
 // Check if the device is a mobile device
 function isMobileDevice() {
-  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
-
-// Apply styles based on device type
-var deviceButton = document.getElementById("deviceButton");
-if (isMobileDevice()) {
-  // Mobile styles
-  deviceButton.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  }
+  
+  // Apply styles based on device type
+  if (isMobileDevice()) {
+    // Mobile styles
     document.getElementById("mobile").style.display = "block";
     document.getElementById("pc").style.display = "none";
-  });
-} else {
-  // PC styles
-  deviceButton.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
+  } else {
+    // PC styles
     document.getElementById("mobile").style.display = "none";
     document.getElementById("pc").style.display = "block";
-  });
-}
+  }  
